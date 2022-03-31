4cac5f4f9a61b78d35d4fcc796cdf40004d5ce46
klavallee <lavallee.kyle@gmail.com>
2022-03-31 15:19:32 -0400

    Add chairs and links

--- androidApp/app/src/main/java/com/example/augmented_reality_app/main_app.java
+++ androidApp/app/src/main/java/com/example/augmented_reality_app/main_app.java
@@ -16,7 +16,19 @@ public class main_app extends AppCompatActivity {
     }
 
     public void goToSo (View view) {
-        goToUrl("https://www.amazon.com/AmazonBasics-High-Back-Executive-Swivel-Puresoft/dp/B07KXF19FN/ref=sr_1_15?crid=2YTBIK4TUNP7T&keywords=black+office+chair&qid=1648663015&sprefix=black+office+chair%2Caps%2C104&sr=8-15");
+        goToUrl("https://www.amazon.com/AmazonBasics-High-Back-Leather-Executive-Computer/dp/B082KYYMZ6/ref=sr_1_13?crid=15MNJC9L1F9PI&keywords=fake%2Bleather%2Boffice%2Bchair&qid=1648752690&sprefix=fake%2Bleather%2Boffice%2Bchair%2Caps%2C63&sr=8-13&th=1");
+    }
+
+    public void goToArozzi (View view) {
+        goToUrl("https://www.amazon.com/Arozzi-Verona-JR-Black-Computer-Gaming-Office/dp/B07C7CK94N/ref=sr_1_24?crid=3JTD6TYE30QQT&keywords=office+chair&qid=1648750564&s=electronics&sprefix=office+chair%2Celectronics%2C98&sr=1-24");
+    }
+
+    public void goToArmless (View view) {
+        goToUrl("https://www.amazon.com/Bowthy-Armless-Ergonomic-Computer-Without/dp/B085C1PHQT/ref=sr_1_47?crid=3MSPM1Q4T5U3J&keywords=armless%2Boffice%2Bchair&qid=1648753331&sprefix=armless%2Boffice%2Bchair%2Caps%2C134&sr=8-47&th=1");
+    }
+
+    public void goToFoldable (View view) {
+        goToUrl("https://www.wayfair.com/school-furniture-and-supplies/pdp/virco-162-162-series-folding-chair-with-plastic-caps-vr10091.html?piid=50111734");
     }
 
     public void goToUrl(String url) {
