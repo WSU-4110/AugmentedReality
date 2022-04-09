945b99d46bc79864c5243f888c3ab43b2241d365
klavallee <lavallee.kyle@gmail.com>
2022-04-09 13:38:08 -0400

    create before annotation

--- androidApp/app/src/test/java/com/example/augmented_reality_app/ExampleUnitTest.java
+++ androidApp/app/src/test/java/com/example/augmented_reality_app/ExampleUnitTest.java
@@ -2,7 +2,13 @@ package com.example.augmented_reality_app;
 
 import static org.junit.Assert.assertEquals;
 
+import android.util.Log;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 
 /**
@@ -12,39 +18,40 @@ import org.junit.Test;
  */
 public class ExampleUnitTest {
 
+    private User t = null;
+
+    @Before
+    public void setupClass() {
+        t = new User();
+    }
+
     @Test
     public void checkAddition() {
-        User t = new User();
         assertEquals(10, t.addition(3, 7));
     }
 
     @Test
     public void checkSubtraction() {
-        User t = new User();
         assertEquals(3, t.subtraction(10, 7));
     }
 
     @Test
     public void checkDivision() {
-        User t = new User();
         assertEquals(10, t.division(30, 3));
     }
 
     @Test
     public void checkMultiplication() {
-        User t = new User();
         assertEquals(25, t.multiplication(5, 5));
     }
 
     @Test
     public void checkPowerOfTwo() {
-        User t = new User();
         assertEquals(36, t.powerOfTwo(6));
     }
 
     @Test
     public void checkSquareRoot() {
-        User t = new User();
         assertEquals(5, t.squareRoot(25), .001);
     }
 
