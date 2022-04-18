9a1a5466d3a4a657d925d35ee3d58a6c446e4344
Ali Said <ali.bilal.said@hotmail.com>
2022-04-17 23:57:47 -0400

    mistaken removed files from python bot

--- webScrape/etsyscrape.py
+++ webScrape/etsyscrape.py
@@ -1,19 +1,41 @@
+from urllib import request
+from wsgiref.headers import Headers
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as ureq
 url = "https://www.etsy.com/search?q=chair"
 
 
-uclient = ureq(url)
-page_html = uclient.read()
-uclient.close()
+class websearcher:
 
 
-page_soup = soup(page_html, "html.parser")
+    def __init__(self):
+        self.url = url
+        self.Headers= Headers
+    
+    def find_container(self):
+        url2 = request.get(self.url , headers=self.Headers)
 
-print(page_soup)
+        # downloads html from page
+        page_soup = soup(url2.content,"lxml")
+        self.productContainer = page_soup.findAll("div", {"class":"TrackedProductCardWrapper"})
 
+    def transfer(self):
+        with open(f"{web_name}.json","w") as outfile:
 
-containers = page_soup.find_all("div" , {"class":"listing-link wt-display-inline-block"})
+            for container in self.productContainer:
+                nameContainer = container.find("div",{"data-enzyme id":"productNameSpaceing"})
+                name = nameContainer.text.strip()
 
-for container in containers:
-    price= container.find("div", {"class":""})
+                priceContainer = container.find("div", {"class":"SFPrice"})
+                price = priceContainer.text.strip()
+
+                imgContainer = container.find("div", {"class":"SFPrice"})
+                img = imgContainer["src"]
+
+                # img["src"] to get image, remember if else , some donot have
+
+                dict = {
+                    "productName" : name,
+                    "price": price,
+                    "image": img
+                }     
